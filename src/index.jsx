3a12f333bc0c2e3f617e3f24bdebf3e003c5dd17
quyenducngo <quyenducngo@gmail.com>
2024-03-13 13:39:39 -0700

    styling my keeper app

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,6 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
-
 // If you're running this locally in VS Code use the commands:
 // npm install
 // to install the node modules and
